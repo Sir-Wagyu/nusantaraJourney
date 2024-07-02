d87699214263e049970ba57890985917043f1bb7
Wahyu NH <141649851+Sir-Wagyu@users.noreply.github.com>
2024-07-02 16:51:09 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      brightness: {
+        80: ".8",
+      },
+      width: {
+        910: "90%",
+      },
+    },
+  },
+  plugins: [],
+};
